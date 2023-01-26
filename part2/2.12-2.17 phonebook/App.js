9290d8a1dad0f9660545a9d9a72bc9ad9f8eee91
benedekkiraly <bendiking@gmail.com>
2023-01-26 21:38:30 +0100

    nbnb

--- part2/2.12-2.17 phonebook/App.js
+++ part2/2.12-2.17 phonebook/App.js
@@ -4,11 +4,25 @@ import SearchBar from './components/SearchBar'
 import SubmitForm from './components/SubmitForm'
 import personService from './services/persons'
 
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className='notification'>
+      {message}
+    </div>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newPhoneNumber, setNewPhone] = useState('')
   const [searchTerm, setSearchTerm ] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+
 
   useEffect(() => {
     console.log('effect')
@@ -49,20 +63,28 @@ const updateNumber = (id,name,newnum) => {
       const foundPerson = persons.find(person=> person.name === personObject.name)
       console.log("idenézz"+foundPerson.id, foundPerson.name, newPhoneNumber)
       updateNumber(foundPerson.id,foundPerson.name,personObject.number)
-      setNewName('')
-      setNewPhone('')
-    }}
+    }
+    setErrorMessage("Updated person with name "+ personObject.name)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 5000)
+    setNewName('')
+    setNewPhone('')}
   else{setPersons(persons.concat(personObject))
     axios
       .post('http://localhost:3001/persons',personObject)
       .then(response => {
-        setPersons(persons.concat(response.data))
-        setNewName('')
-        setNewPhone('')
-      })
+        setPersons(persons.concat(response.data))})
+    
     console.log("Added person")
+    setErrorMessage("Person added with name "+ personObject.name)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 5000)
+    setNewName('')
+    setNewPhone('')
   }
-}
+  }
 
 const deletePerson = id => {
   if(window.confirm("Do you really want to remove user with id " + " ?")){
@@ -76,7 +98,6 @@ const deletePerson = id => {
       )
     })
   setPersons(persons.filter(person => person.id !== id))
-  console.log("lol" + persons)
 }
 
 
@@ -105,6 +126,7 @@ const deletePerson = id => {
 
   return (
     <div>
+      <Notification message={errorMessage} />
       <h2>Phonebook</h2>
       <div>
         <SearchBar value = {searchTerm} onChange={handleSearch} />
